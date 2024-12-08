22b99d58859b84c0b411163a9b9e1ca126015cbf
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2024-12-08 11:03:22 -0300

    Entrega: Desafio Modelo de Dominio e ORM

--- src/main/java/com/devsuperior/dominioeorm/entities/Categoria.java
+++ src/main/java/com/devsuperior/dominioeorm/entities/Categoria.java
@@ -1,5 +1,83 @@
 package com.devsuperior.dominioeorm.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
 public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(columnDefinition = "TEXT") //campo description na tabela é definida como TEXT ao invés de VARCHAR, pois TEXT recebe quantidade maior de dados.
+	private String descricao;
+	
+	@OneToMany(mappedBy = "categoria")
+	private List<Atividade> atividades = new ArrayList<>();
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Long id, String descricao) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public List<Atividade> getAtividades() {
+		return atividades;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", descricao=" + descricao + "]";
+	}
+	
+	
 
 }
--- src/main/java/com/devsuperior/dominioeorm/entities/Participante.java
+++ src/main/java/com/devsuperior/dominioeorm/entities/Participante.java
@@ -1,5 +1,92 @@
 package com.devsuperior.dominioeorm.entities;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_participante")
 public class Participante {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	@Column(columnDefinition = "TEXT") //campo description na tabela é definida como TEXT ao invés de VARCHAR, pois TEXT recebe quantidade maior de dados.
+	private String email;
+	
+	@ManyToMany(mappedBy = "participantes")
+	private Set<Atividade> atividades = new HashSet<>();
+	
+	public Participante() {
+		
+	}
+
+	public Participante(Long id, String nome, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Set<Atividade> getAtividade() {
+		return atividades;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Participante other = (Participante) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Participante [id=" + id + ", name=" + nome + ", email=" + email + "]";
+	}
+	
+	
+	
 }
